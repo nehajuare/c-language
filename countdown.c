dcad5663044a16d8e9f5954243bd658c344241bd
Neha Juare <neha.juare207@gmail.com>
2026-04-28 13:24:11 +0530

    321 count down

---
+++ countdown.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int countdown = 3;
+  for(countdown = 3 ;countdown > 0; countdown-- ;)
+  {
+    printf("%d\n",countdown);
+   
+  }
+   printf("happy new year!!");
+   return 0;
+}
