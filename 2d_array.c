fa3dfb0ca99588da7e1c079d10105d02df7111eb
Neha Juare <neha.juare207@gmail.com>
2026-04-28 12:56:32 +0530

    2 dimentional array

---
+++ 2d_array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a[2][2],i,j;
+	printf("enter the elements\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("matrix=\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+		   printf("  %d",a[i][j]);
+    	}
+    	printf("\n");
+		
+	}
+
+	return 0;
+}
