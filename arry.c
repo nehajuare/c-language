75049c184e9d96bc76910a91a99192d33d5766b2
Neha Juare <neha.juare207@gmail.com>
2026-04-28 13:17:01 +0530

    Add files via upload

---
+++ arry.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int arr[5] ={45,56,75,54,90};
+	printf("english %d\n",arr[0]);
+	printf("math %d\n",arr[1]);
+	printf("marathi %d\n",arr[2]);
+	printf("sience %d\n",arr[3]);
+	printf("hindi %d\n",arr[4]);
+	return 0;
+}
