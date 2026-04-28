681678aff45a039cf750b2180320c251b5e5b1b3
Neha Juare <neha.juare207@gmail.com>
2026-04-28 13:15:57 +0530

    array1

---
+++ array1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int arr[5];
+	int i;
+	printf("enter the no\n");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&arr[i]);
+
+	}
+	for(i=0;i<5;i++)
+
+	{
+		printf("array of element%d\n",arr[i]);
+    }
+	return 0;
+}
