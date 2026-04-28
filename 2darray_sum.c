7ab0771fc4e8edd02339701fa2211ad0de4111ef
Neha Juare <neha.juare207@gmail.com>
2026-04-28 13:06:30 +0530

    2 dimentional array addition

---
+++ 2darray_sum.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+	int a[2][2],b[2][2],sum[2][2],i,j;
+	printf("enter the 1 element=\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+	
+	   {
+		scanf("%d",&a[i][j]);
+    	}
+    } 
+    printf("enter the 2element=\n");
+
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+		  scanf("%d",&b[i][j]);
+		}
+	}
+
+	printf("sum");
+
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			sum[i][j]=a[i][j]+b[i][j];
+		}
+	}
+	printf("sum of element=\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("  %d",sum[i][j]);
+		
+		}
+			printf("\n");
+	}
+	return 0;
+}
