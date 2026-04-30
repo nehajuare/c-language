461426222ecd2a1a4cab0b8646ec2bc80b00da3a
Neha Juare <neha.juare207@gmail.com>
2026-04-30 15:28:20 +0530

    Add files via upload

---
+++ nested_if_marks.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int marks;
+	printf("enter the marks");
+	scanf("%d",&marks);
+	if(marks>=90)
+	{
+		printf("1st class");
+	}
+	else if(marks>=70)
+	{
+		printf("2nd class");
+	}
+	else if(marks>=50)
+	{
+		printf("3rd class");
+	}
+	else if(marks >=35)
+	{
+		printf("pass");
+	}
+	else
+	{
+		printf("fail");
+	}
+	return 0;
+}
