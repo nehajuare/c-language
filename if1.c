2912b092f633dec214f365514d52dc04bb013062
Neha Juare <neha.juare207@gmail.com>
2026-04-30 15:23:00 +0530

    Add files via upload

---
+++ if1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int no;
+	printf("enter the no=");
+	scanf("%d",&no);
+	if(no%2==0)
+	{printf("this is even no");
+	}
+	else
+	{printf("this is odd no");
+	}
+	return 0;
+
+	
+}
